af0b9e0e41875355f15f9edc533c48e60ef7f028
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-10-13 21:16:46 +0900

    ボックスの登録方法を表示

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ vagrant  halt
 vagrant  package   --output ../ubuntu-develop.box  default
 
 popd
+set  +x
+
+cat  << _EOF_  1>&2
+To add package into box list, run:
+_EOF_
+
+cat  << _EOF_
+vagrant  box  add  --name ubuntu/bionic64-develop   ubuntu-develop.box
+_EOF_
