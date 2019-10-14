be17c13c40b7acefa99c2c383b3f4cea90bfea22
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-10-14 12:50:38 +0900

    パッケージインストール後の後始末を追加

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -9,3 +9,8 @@ sudo  apt  update
 sudo  apt-mark   hold  libssl1.1
 sudo  apt  upgrade -y
 sudo  apt  install -y  build-essential  mercurial
+
+# clean up
+sudo  rm -f  /var/lib/apt/lists/lock
+sudo  rm -f  /var/cache/apt/archives/lock
+sudo  rm -f  /var/lib/dpkg/lock
