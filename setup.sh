e877cf12c06f31e16a79eb1d9e64a4e2436512a2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-12-30 19:34:05 +0900

    ボックス名を変更

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ vagrant  destroy -f  || exit $?
 vagrant  up          || exit $?
 vagrant  halt
 
-vagrant  package   --output ../ubuntu-develop.box  default
+vagrant  package   --output ../ubuntu-pbspro.box  default
 
 popd
 set  +x
@@ -16,5 +16,5 @@ To add package into box list, run:
 _EOF_
 
 cat  << _EOF_
-vagrant  box  add  --name ubuntu/bionic64-develop   ubuntu-develop.box
+vagrant  box  add  --name ubuntu/bionic64-pbspro   ubuntu-pbspro.box
 _EOF_
