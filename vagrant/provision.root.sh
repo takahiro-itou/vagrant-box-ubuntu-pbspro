ae0172786e5dd6618d2a50f8a1ba766d059a1d0e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-12-30 19:34:20 +0900

    パッケージをインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -10,6 +10,15 @@ sudo  apt-mark   hold  libssl1.1
 sudo  apt  upgrade -y
 sudo  apt  install -y  build-essential  mercurial
 
+sudo  apt  install -y   git  emacs  \
+    gcc  make  libtool  libhwloc-dev  libx11-dev            \
+    libxt-dev  libedit-dev  libical-dev  ncurses-dev        \
+    perl  postgresql-server-dev-all  postgresql-contrib     \
+    python3-dev  tcl-dev  tk-dev  swig  libexpat-dev        \
+    libssl-dev   libxext-dev  libxft-dev autoconf automake  \
+    expat  libedit2  postgresql  python3  sendmail-bin      \
+    sudo   tcl  tk   libical1a   time
+
 # clean up
 sudo  rm -f  /var/lib/apt/lists/lock
 sudo  rm -f  /var/cache/apt/archives/lock
