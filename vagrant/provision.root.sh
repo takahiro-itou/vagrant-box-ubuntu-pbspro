f63368c23ce4091d90d2b54cfbe9c9a4a7d2102d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-12 13:28:16 +0900

    PBSPro 18.1.4 をインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -14,10 +14,22 @@ sudo  -E  apt  install -y   git  emacs  \
     gcc  make  libtool  libhwloc-dev  libx11-dev            \
     libxt-dev  libedit-dev  libical-dev  ncurses-dev        \
     perl  postgresql-server-dev-all  postgresql-contrib     \
-    python3-dev  tcl-dev  tk-dev  swig  libexpat-dev        \
+    python-dev  tcl-dev  tk-dev  swig  libexpat-dev         \
     libssl-dev   libxext-dev  libxft-dev autoconf automake  \
-    expat  libedit2  postgresql  python3  sendmail-bin      \
+    expat  libedit2  postgresql  python  sendmail-bin       \
     sudo   tcl  tk   libical3    time
+
+# Install pbspro v18.1.4
+git  clone  https://github.com/PBSPro/pbspro.git  -b v18.1.4
+pushd  pbspro
+./autogen.sh
+./configure  --prefix=/opt/pbs/
+make
+sudo  make  install
+popd
+
+sudo  /opt/pbs/libexec/pbs_postinstall
+sudo  chmod  4755  /opt/pbs/sbin/pbs_iff  /opt/pbs/sbin/pbs_rcp
 
 # clean up
 sudo  rm -f  /var/lib/apt/lists/lock
